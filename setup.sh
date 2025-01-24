ce52df1942899a73457a3b624ff79b7ef93ab043
Robert Boyd III <git@robertboyd.dev>
2025-01-23 19:26:51 -0800

    .

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 dnf install git -y
 
 git clone --bare https://github.com/rob-3/dotfiles "$HOME/.dotfiles"
-git checkout --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"
+git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" checkout 
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --diagnostic-endpoint="" --no-confirm
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
