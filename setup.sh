df11b36ae68f397d393fc736a6f2fbf4c5d050ce
Robert Boyd III <git@robertboyd.dev>
2025-02-09 21:02:15 -0800

    Add support for systems without git

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone --bare https://github.com/rob-3/dotfiles "$HOME/.dotfiles"
-git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" checkout 
-
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --diagnostic-endpoint="" --no-confirm
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+nix run nixpkgs#git -- clone --bare https://github.com/rob-3/dotfiles "$HOME/.dotfiles"
+nix run nixpkgs#git -- --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" checkout
 
 cd "$HOME/.config/nix" || exit
 nix profile install
