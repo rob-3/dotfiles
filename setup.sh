a6421bf7b429f81c7d91ed80f4f9d5d4f5952213
Robert Boyd III <git@robertboyd.dev>
2025-03-30 15:24:47 -0700

    .

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 nix run nixpkgs#git -- clone --bare https://github.com/rob-3/dotfiles "$HOME/.dotfiles"
+nix run nixpkgs#git -- --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" switch phone
 nix run nixpkgs#git -- --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" checkout
 
 cd "$HOME/.config/nix" || exit
